5e17d449902bbe9c7880b4213e83d87c4f53364a
Philip Van de Vyver <vande119@cronos.be>
2023-05-08 10:19:11 +0200

    init

---
+++ Policy/Definitions/appservice_msi.tf
@@ -0,0 +1,43 @@
+resource "azurerm_policy_definition" "locationpolicy" {
+  name         = "locationpolicy"
+  policy_type  = "Custom"
+  mode         = "Indexed"
+  display_name = "location policy definition"
+
+  metadata = <<METADATA
+    {
+    "category": "General"
+    }
+
+METADATA
+
+
+  policy_rule = <<POLICY_RULE
+ {
+    "if": {
+      "not": {
+        "field": "location",
+        "in": "[parameters('allowedLocations')]"
+      }
+    },
+    "then": {
+      "effect": "audit"
+    }
+  }
+POLICY_RULE
+
+
+  parameters = <<PARAMETERS
+ {
+    "allowedLocations": {
+      "type": "Array",
+      "metadata": {
+        "description": "The list of allowed locations for resources.",
+        "displayName": "Allowed locations",
+        "strongType": "location"
+      }
+    }
+  }
+PARAMETERS
+
+}
---
+++ Policy/Definitions/azure.tf
@@ -0,0 +1,5 @@
+provider "azurerm" {
+  version = "=2.3.0"
+  subscription_id = "73d1976a-3d59-4c49-948e-847f08f43071"
+features {}
+}
