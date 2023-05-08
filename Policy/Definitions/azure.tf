6d4d29cd043d941ca553145f3d248fee9c3feef2
Philip Van de Vyver <vande119@cronos.be>
2023-05-08 11:36:12 +0200

    tf

--- Policy/Definitions/azure.tf
+++ Policy/Definitions/azure.tf
@@ -1,5 +1,16 @@
-provider "azurerm" {
-  version = "=2.3.0"
-  subscription_id = "73d1976a-3d59-4c49-948e-847f08f43071"
-features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.30.0"
+    }
+  }
 }
+
+provider "azurerm" {
+  use_oidc = true
+  skip_provider_registration = true
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
